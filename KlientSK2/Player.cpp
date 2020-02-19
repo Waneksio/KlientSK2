182f3c999301cfc3bef9b8f0ff553aa8d131200a
Waneksio <waneksiomiszczu@gmail.com>
2020-02-19 12:42:46 +0100

    Add messages storage

---
+++ KlientSK2/Player.cpp
@@ -0,0 +1,5 @@
+//
+// Created by matt on 18.02.20.
+//
+
+#include "Player.h"
---
+++ KlientSK2/Player.h
@@ -0,0 +1,14 @@
+//
+// Created by matt on 18.02.20.
+//
+
+#ifndef KLIENTSK2_PLAYER_H
+#define KLIENTSK2_PLAYER_H
+
+
+class Player {
+
+};
+
+
+#endif //KLIENTSK2_PLAYER_H
